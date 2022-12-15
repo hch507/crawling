4b0b265665edf92113fa6cf386362f9a574b4639
chungHee Han <chunghuihan9@gmail.com>
2022-12-15 20:57:36 +0900

    first commit

--- crawling.py
+++ crawling.py
@@ -2,6 +2,6 @@ from google_images_download import google_images_download   #importing the libra
 
 response = google_images_download.googleimagesdownload()   #class instantiation
 
-arguments = {"keywords":"Polar bears,baloons,Beaches","limit":20,"print_urls":True,"format":"jpg"}   #creating list of arguments
+arguments = {"keywords":"행복한 표정,슬픈 표정","limit":100,"print_urls":True,"format":"jpg"}   #creating list of arguments
 paths = response.download(arguments)   #passing the arguments to the function
 print(paths)   #printing absolute paths of the downloaded image
